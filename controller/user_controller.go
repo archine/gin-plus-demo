3bb25bb0aae1f8f3f4210365c2493033b78dc177
gjing <me@gjing.cn>
2023-02-27 15:36:15 +0800

    gin-plus升级到v2.1.0

---
+++ controller/user_controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"gin-plus-demo/model"
+	"github.com/archine/gin-plus/v2/mvc"
+	"github.com/archine/gin-plus/v2/resp"
+	"github.com/gin-gonic/gin"
+)
+
+// UserController
+// @BasePath("/user")
+type UserController struct {
+	mvc.Controller
+	UserMapper *model.UserMapper
+}
+
+// User
+// @GET(path="/") 获取用户
+func (u *UserController) User(ctx *gin.Context) {
+	resp.Json(ctx, u.UserMapper.GetUser())
+}
+
+// AddUser
+// @POST(path="/") 添加用户
+func (u *UserController) AddUser(ctx *gin.Context) {
+	var arg model.User
+	if resp.ParamValid(ctx, ctx.ShouldBindJSON(&arg), &arg) {
+		return
+	}
+	u.UserMapper.AddUser(&arg)
+	resp.Ok(ctx)
+}
--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"gin-plus-demo/base"
 	_ "gin-plus-demo/controller"
-	"github.com/archine/gin-plus/v2/ast"
-	"github.com/archine/gin-plus/v2/mvc"
-	log "github.com/sirupsen/logrus"
-	"os"
+	"github.com/archine/gin-plus/v2/application"
 )
 
-//go:generate go run main.go ast
+//go:generate mvc
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "ast" {
-		ast.Parse("controller")
-		return
-	}
-	app, err := base.NewApp("app.yml")
-	if err != nil {
-		log.Fatalf("initializer application failed,%s", err.Error())
-	}
-	mvc.Apply(app.Engine, true, base.Ast)
-	app.Run()
+	application.Default().Run()
 }
